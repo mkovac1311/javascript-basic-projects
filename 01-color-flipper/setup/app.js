fa0bcd43fb8ce74aa6636f93864929e911c08802
mkovac1311 <kovac.mitja1@gmail.com>
2024-05-23 13:15:41 +0200

    update

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const button = document.querySelector("#btn");
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", (e) => {
+  console.log("click");
+  const getRandomNo = Math.floor(Math.random() * colors.length);
+  console.log(getRandomNo);
+  document.body.style.backgroundColor = colors[getRandomNo];
+  color.textContent = colors[getRandomNo];
+});
